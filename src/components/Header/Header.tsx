841cfc125003269f859d74930a640ff736c816ae
Marlon Gómez <marlonjulian15@gmail.com>
2021-09-18 15:12:12 -0500

    Structure and styles of Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./header.css";
+
+export const Header = () => {
+  return (
+    <header className="container">
+      <h2>Electronics</h2>
+    </header>
+  );
+};
