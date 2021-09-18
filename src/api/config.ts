b944dd139499b71e38ca3a0bd85d858651435cb3
Marlon Gómez <marlonjulian15@gmail.com>
2021-09-17 22:02:12 -0500

    Add variables API

---
+++ src/api/config.ts
@@ -0,0 +1,3 @@
+
+export const basePath:string = "https://coding-challenge-api.aerolab.co/";
+export const tokenAPI:string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTQ1NGNmYWE1MjkwZjAwMjE3ODExMTIiLCJpYXQiOjE2MzE5MzE2NDJ9.MBpyBGoxL5LLCr6eJsfZdJW3n-xEtqFozA97kERYzCg";
